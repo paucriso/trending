4db314353a3e20fb7d75f37f8687033ce801f4f4
Paulo Otero <digitweb.mx@gmail.com>
2020-10-27 16:25:30 -0600

    le instale swiper

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      loop: true
+    });
+  }
 
   ngOnInit(): void {
   }
