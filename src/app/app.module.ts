d00b40bff206d1d5e8902cae1f4caf109df543c8
Paulo Otero <digitweb.mx@gmail.com>
2020-10-28 14:53:29 -0600

    empezando home y svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
